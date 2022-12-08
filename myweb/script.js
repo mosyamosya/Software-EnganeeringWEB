d53815163d92186e46917395151c36b177f4507d
mosyamosya <93034139+mosyamosya@users.noreply.github.com>
2022-12-08 20:15:18 +0200

    Add files via upload
    practic and project

---
+++ myweb/script.js
@@ -0,0 +1,14 @@
+/* global VANTA */
+VANTA.NET({
+  el: '#ahead',
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x4a2f,
+  backgroundColor: 0x0,
+  maxDistance: 22.00,
+});
